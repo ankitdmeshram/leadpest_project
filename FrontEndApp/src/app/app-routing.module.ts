66769213088a8b2cafb2dd32ce9c132707a1c6f6
Ankit Meshram <ankitdm69@gmail.com>
2023-05-28 22:06:39 +0530

    F-I2 - Signin component and design created, routers sets for signin page

--- FrontEndApp/src/app/app-routing.module.ts
+++ FrontEndApp/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './authentication/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: SigninComponent
+  },
+  {
+    path: "signin",
+    component: SigninComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
