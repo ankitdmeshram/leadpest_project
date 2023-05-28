9ef31641ad9833fcaf217fa4329e26580c5b6fb6
Ankit Meshram <ankitdm69@gmail.com>
2023-05-28 22:10:06 +0530

    F-I2 - Signup page created

--- FrontEndApp/src/app/app-routing.module.ts
+++ FrontEndApp/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './authentication/signin/signin.component';
+import { SignupComponent } from './authentication/signup/signup.component';
 
 const routes: Routes = [
-  {
-    path: "",
-    component: SigninComponent
-  },
+
   {
     path: "signin",
     component: SigninComponent
+  },
+  {
+    path: "signup",
+    component: SignupComponent
   },
 
 ];
---
+++ FrontEndApp/src/app/authentication/signup/signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css', '../auth.component.css']
+})
+export class SignupComponent {
+
+}
